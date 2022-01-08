f1b25a882b1039a806043161f556950385d38a5d
Tobiasz Nogaj <tobnog@hotmail.com>
2022-01-08 03:01:06 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/git_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_01.dir/main.cpp.obj"
+  "git_01.exe"
+  "git_01.exe.manifest"
+  "git_01.pdb"
+  "libgit_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
